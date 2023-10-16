68cc66cb67bc0ad39aef94b6963550ab05d14969
filipwiniarski <filip@neversolo.gg>
2023-10-16 18:10:09 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 export class AppComponent {
   morphs: string[][] = [['/assets/img/i1.png', '/assets/img/i2.png']];
 
-  //one = ['/assets/img/o1.png', '/assets/img/o2.png', '/assets/img/o3.png'];
-
-  //two = ['/assets/img/o1.png', '/assets/img/o2.png', '/assets/img/o3.png'];
-
   onMorph(morphOutput: string[]): void {
     this.morphs.push(morphOutput);
   }
--- src/app/core/input-image/input-image.component.ts
+++ src/app/core/input-image/input-image.component.ts
@@ -1,12 +1,27 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-input-image',
   templateUrl: './input-image.component.html',
   styleUrls: ['./input-image.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'block',
+  },
 })
 export class InputImageComponent {
   @Input()
   imageSrc: string | undefined;
+
+  @HostBinding('style.filter')
+  hue;
+
+  constructor() {
+    this.hue = `hue-rotate(${360 * Math.random()}deg)`;
+  }
 }
